e89ec78d808118ea94ee74165839eb1696c72615
USR <usr@example.com>
2026-09-01 19:34:11 +0100

    Stage remaining changes: env, lockfile, prisma script

---
+++ run-prisma.bat
@@ -0,0 +1,4 @@
+@echo off
+set MONGODB_URI=mongodb+srv://zeekthegoated_db_user:C8LO3XZvAQmhOkq6@theta.ej3gzsx.mongodb.net/theta_shard2?retryWrites=true&w=majority&appName=Theta
+cd /d C:\Users\USR\021-copy\Theta
+npx.cmd prisma db push
